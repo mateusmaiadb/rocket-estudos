03a6af613d8e4332be66c3ef0b1df585a91f5230
Mateus Pereira <mateus.maia@dbserver.com.br>
2023-01-13 18:01:55 -0300

    teste

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// let imgSofa = "img/sofa.svg";
-// let imgSofaTresD = "img/sofa.gif"
+let imgSofa = "img/sofa.svg";
+let imgSofaTresD = "img/sofa.gif"
 
 
-// function trocar(){
+function trocar(){
 
-//     document.getElementById("figura").src = imgSofaTresD;
+    document.getElementById("figura").src = imgSofaTresD;
 
-//     let aux = imgSofa;
-//     imgSofa = imgSofaTresD;
-//     imgSofaTresD = aux;
+    let aux = imgSofa;
+    imgSofa = imgSofaTresD;
+    imgSofaTresD = aux;
 
-// }
+}
 
 
-let img = document.getElementById("figura")
+// let img = document.getElementById("figura")
 
-img.addEventListener("mouseover", function(){
-    this.src = "img/sofa.gif";
-})
+// img.addEventListener("mouseover", function(){
+//     this.src = "img/sofa.gif";
+// })
 
-img.addEventListener("mouseout", function(){
-    this.src = "img/sofa.svg";
-})
+// img.addEventListener("mouseout", function(){
+//     this.src = "img/sofa.svg";
+// })
 
 
 
